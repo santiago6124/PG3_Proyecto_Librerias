b3768a956f01656af3d78f8c1abe196c9be855d7
Francisco Brezzo <fran.brezzo@gmail.com>
2022-05-20 11:58:58 -0300

    Implementacion de grafico con binance

--- ta.py
+++ ta.py
@@ -1,30 +1,27 @@
 import ccxt, yfinance
 import pandas_ta as ta
 import pandas as pd
+import matplotlib.pyplot as plt
 
 exchange = ccxt.binance()
-
-bars = exchange.fetch_ohlcv('ETH/USDT', timeframe='5m', limit=500)
-
+bars = exchange.fetch_ohlcv('ETH/USDT', timeframe='1h', limit=100)
 df = pd.DataFrame(bars, columns=['time', 'open', 'high', 'low', 'close', 'volume'])
-
-print(df)
-
 
 
 adx = df.ta.adx()
-
 macd = df.ta.macd(fast=14, slow=28)
 
-print(macd)
 
 rsi = df.ta.rsi()
-
 df = pd.concat([df, adx, macd, rsi], axis=1)
-
-print(df)
-
 df = df[df['RSI_14'] < 30]
+df.ta.sma(length = 100, append = True)
 
+
+suma = 0
+#ahora cada vez que llamen hay que subirle un numerito
+
+plt.plot(rsi.values)
+plt.savefig('grafico'+ suma +'.png')
+plt.show()
 print(df)
-
