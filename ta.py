7306660a1da8428bbd1f5b366e491554edace8e8
Santiago Carranza <santiagocarranzazinny@gmail.com>
2022-05-21 15:22:12 -0300

    agregada la función de hacer el gráfico desde un archivo externo

--- ta.py
+++ ta.py
@@ -22,6 +22,18 @@ suma = 0
 #ahora cada vez que llamen hay que subirle un numerito
 
 plt.plot(rsi.values)
-plt.savefig('grafico'+ suma +'.png')
+plt.savefig('grafico.png')
 plt.show()
 print(df)
+
+#ahora para que se haga el gráfico desde un archivo externo
+
+dt = pd.read_csv("btc.csv")[["unix","open", "high","low", "close"]]
+dt.sort_values(by="unix", inplace=True)
+dt["date"] = pd.to_datetime(dt["unix"], unit = 's')
+
+dt.ta.sma(length = 100, append = True)
+
+plt.plot(dt.date, dt.close)
+plt.plot(dt.date, dt.SMA_100)
+plt.show()
